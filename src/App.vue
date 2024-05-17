b963d9f2489842ecfa97d20133242f5c9f50ec67
Nicolas Arias <101823845+NicolasAriasUfro@users.noreply.github.com>
2024-05-17 15:46:55 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "@/components/TodoList.vue";
+</script>
+
+<template>
+  <TodoList></TodoList>
+</template>
+
+<style scoped>
+</style>
