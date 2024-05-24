65159a1e5380d4c88b7cbcac77e712b6f240f0c9
Nicolas Arias <101823845+NicolasAriasUfro@users.noreply.github.com>
2024-05-24 16:22:54 -0400

    parte 1

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import TodoList from "@/components/TodoList.vue";
+import TodoList from "@/client/components/TodoList.vue";
 </script>
 
 <template>
--- src/client/components/TodoList.vue
+++ src/client/components/TodoList.vue
@@ -36,6 +36,9 @@ index" class="form-check-input">
       </div>
     </div>
   </div>
+
+  <h2 v-if="mensaje.length>0">{{mensaje}}</h2>
+  <h3>mensaje = {{mensaje}}</h3>
 </template>
 <script>
 import Hijo from './Hijo.vue';
@@ -53,15 +56,49 @@ export default {
         {nombre: 'Tarea 2', completada: false},
         {nombre: 'Tarea 3', completada: false}
       ],
-      claseSeleccionada: 'text-decoration: line-through ' // Clase CSS inicial
+      claseSeleccionada: 'text-decoration: line-through ', // Clase CSS inicial
+      mensaje: '',
     }
   },
   methods: {
     resetearMensaje() {
       this.mensajePadre = 'Hola desde el padre'; // Método para resetear el mensaje del padre
 
-    }
-  }
+    },
+    obtenerMensaje(){
+      fetch('http://localhost:3000/hello')
+          .then(response => response.text())
+          .then(data=> {
+            this.mensaje = data
+          });
+    },
+    fetchTareas(){
+      fetch('http://localhost:3000/tareas')
+          .then(response => response.json())
+          .then(data=>{
+            this.tareas = data;
+          });
+    },
+    actualizarTarea(index) {
+      fetch('http://localhost:3000/tareas', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          index: index,
+          completada: this.tareas[index].completada
+        }),
+      })
+          .then(response => response.json())
+          .then(data => {
+            console.log(`Tarea ${index} actualizada:${data.completada}`);
+          });
+  },
+  created() {
+    this.obtenerMensaje()
+  },
+}
 }
 </script>
 <style>
